7e2d528ef4cf28c2d293293151e6178fa2fea494
Rashedul Hasan <Rashedul Hasan>
2021-12-01 00:06:03 +0600

    Simple JavaScript

---
+++ index.js
@@ -0,0 +1,70 @@
+//Function
+// function myLove(){
+//     console.log("Bangladesh1");
+//     console.log("Bangladesh2");
+//     console.log("Bangladesh3");
+//     console.log("Bangladesh4");
+// }
+// myLove();
+// myLove();
+// myLove();
+// var num = 2;
+// console.log(num);
+// myLove();
+
+// function doubleIt(num){
+//     var result =num*2;
+//     console.log(result);
+//     // return result;
+// }
+// doubleIt(4); 
+// doubleIt(5); 
+// doubleIt(6); 
+// doubleIt(7); 
+// doubleIt(8); 
+
+// function doubelIt(num){
+//     var result = num*2;
+//     return result;
+// }
+
+// var firstNumber = doubelIt(2); //4
+// var secondNumber = doubelIt(3); //6
+// var result = firstNumber+secondNumber; //4+6=10
+// console.log("The result is:",result); //10
+
+// function mathOperations(num1,num2){
+//     var add= num1+num2; //10+3 = 13
+//     var sub= num1-num2; //10-3 = 7
+//     var multi = num1*num2; //10*3 = 30
+//     var div = num1/num2; //10/3 = 3
+//     var remi = num1%num2; // 10%3 = 1
+//     return [add,sub,multi,div,remi];
+
+// }
+// var [add,sub,multi,div,remi] = mathOperations(10,3);
+// console.log("The Add number is:",add);
+// console.log("The Sub number is:",sub);
+// console.log("The Multi number is:",multi);
+// console.log("The Div number is:",div);
+// console.log("The Remi number is:",remi);
+
+
+//Object
+var student1 = {id:123, phone:2134, name:"tapu"};
+var student2 = {id:124, phone:3466, name:"rakib"};
+var student3 = {id:125, phone:4677, name:"srowal"};
+
+var latestId = student3.id = 121;
+console.log(latestId);
+console.log(student3);
+
+// var idNumber = student1["id"];
+// console.log(idNumber);
+
+// var idNumber = student1.id;
+// console.log(idNumber);
+
+
+// console.log(student1);
+// console.log(student2);
